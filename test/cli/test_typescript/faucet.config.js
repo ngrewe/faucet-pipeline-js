e177e0d1d8f6e5a24bec70f2cfdd5a879c36d1f6
FND <fnd@localhost.localdomain>
2018-12-25 11:16:12 +0100

    WIP: added TypeScript configuration abstractions
    test are still incomplete and don't comprehensively cover all options
    
    also note inline comments

--- test/cli/test_typescript/faucet.config.js
+++ test/cli/test_typescript/faucet.config.js
@@ -7,6 +7,19 @@ module.exports = {
 		source: "./src/index.ts",
 		target: "./dist/bundle.js",
 		typescript: true
+	}, {
+		source: "./src/index.ts",
+		target: "./dist/bundle_es5.js",
+		typescript: {
+			transpile: true,
+			strict: true
+		}
+	}, {
+		source: "./src/index.tsx",
+		target: "./dist/bundle_jsx.js",
+		typescript: {
+			jsx: "createElement"
+		}
 	}],
 	plugins: {
 		js: {
